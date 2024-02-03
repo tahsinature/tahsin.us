98fb288d52db7bf3ee70639484854d0b08fbffc8
Tahsin <work.tahsin@gmail.com>
2024-02-03 10:26:53 -0500

    change gallery metadata

--- src/app/photographs/page.tsx
+++ src/app/photographs/page.tsx
@@ -1,18 +1,19 @@
+import { Metadata } from "next";
 import Head from "next/head";
 
 import Gallery from "@/components/Gallery/Gallery";
 import services from "@/services";
+
+export const metadata: Metadata = {
+  title: "Gallery | tahsin.us",
+  description: "All photographs from Tahsin",
+};
 
 const Page = async () => {
   const photosFromServer = await services.gallery.getImages();
 
   return (
     <div className="pb-10">
-      <Head>
-        <title>Gallery</title>
-        <meta name="description" content="Tahsin Photography" />
-      </Head>
-
       <Gallery photosFromServer={photosFromServer} />
     </div>
   );
