78345072ce10bd519cfc067092c7f7354e9716d3
Tahsin <work.tahsin@gmail.com>
2024-02-03 10:26:54 -0500

    add prod s3 bucket url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["source.unsplash.com", "picsum.photos", "prod-files-secure.s3.us-west-2.amazonaws.com", "prod-files-secure.s3.us-west-1.amazonaws.com"],
+  },
+};
 
 export default nextConfig;
