256d64d6c412ed68d316a2abd4869ece4ca4b493
Tahsin <work.tahsin@gmail.com>
2024-02-03 10:26:52 -0500

    add dummy gallery service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
 import * as language from "./language";
 import * as work from "./work";
+import * as gallery from "./gallery";
 
 const services = {
   language,
   work,
+  gallery,
 };
 
 export default services;
