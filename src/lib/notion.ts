12d50f2a2f04d5d0eccd4161e8f713c8a97d96df
Tahsin <work.tahsin@gmail.com>
2024-02-04 22:45:49 -0500

    update notion query lib

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -9,10 +9,13 @@ export const notion = new Client({
 
 export const database_ids = {
   language: "unset",
-  work: "a958ff8e06b54db19f173483adff89f9",
   gallery: "unset",
+  work: "a958ff8e06b54db19f173483adff89f9",
   blog: "efec0bbfb0a7436f9fca047b4efd9223",
   tags: "7e0ddc0661d7414a9051ce26db4cedea",
+  toolsIUse: "1e7e2155cabb40d5bc8df166a452b2f5",
+  programmingLanguage: "f7f3f6416d914904b9fd3bdc4e99cf1d",
+  tempImage: "40498135f89643caa330401a143a9aa3",
 };
 
 export const queryFromDB = async (schema: NotionORMSchema, database_id: string) => {
@@ -26,23 +29,28 @@ export const queryFromDB = async (schema: NotionORMSchema, database_id: string) 
     Object.keys(schema.properties).forEach((key) => {
       const property = schema.properties[key];
       const dbProperty = item.properties[key];
+      const finalKey = property.transformKey || key;
 
       if (!dbProperty) return;
 
       if (property.type === "title") {
-        finalItem[key] = _.get(item, `properties.${key}.title[0].plain_text`);
+        finalItem[finalKey] = _.get(item, `properties.${key}.title[0].plain_text`);
       } else if (property.type === "rich_text") {
-        finalItem[key] = _.get(item, `properties.${key}.rich_text[0].plain_text`);
+        finalItem[finalKey] = _.get(item, `properties.${key}.rich_text[0].plain_text`);
       } else if (property.type === "files") {
         if (property.subType === "url") {
-          finalItem[key] = _.get(item, `properties.${key}.files`, []).map((item: any) => item.file.url);
+          finalItem[finalKey] = _.get(item, `properties.${key}.files`, []).map((item: any) => item.file.url);
         } else if (property.subType === "external") {
-          finalItem[key] = _.get(item, `properties.${key}.files`, []).map((item: any) => item.external.url);
+          finalItem[finalKey] = _.get(item, `properties.${key}.files`, []).map((item: any) => item.external.url);
         }
       } else if (property.type === "relation") {
-        finalItem[key] = _.get(item, `properties.${key}.relation`).map((item: any) => item.id);
+        finalItem[finalKey] = _.get(item, `properties.${key}.relation`).map((item: any) => item.id);
       } else if (property.type === "checkbox") {
-        finalItem[key] = _.get(item, `properties.${key}.checkbox`);
+        finalItem[finalKey] = _.get(item, `properties.${key}.checkbox`);
+      } else if (property.type === "unique_id") {
+        finalItem[finalKey] = _.get(item, `properties.${key}.unique_id.number`);
+      } else if (property.type === "date") {
+        finalItem[finalKey] = { start: _.get(item, `properties.${key}.date.start`), end: _.get(item, `properties.${key}.date.end`) };
       }
     });
 
