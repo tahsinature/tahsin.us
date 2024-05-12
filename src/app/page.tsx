48776922a804be79a947f0d1399699ec1f5b51da
Tahsin <work.tahsin@gmail.com>
2024-05-12 13:42:58 -0400

    remove language i speak from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import * as React from "react";
 
-import LanguageSpeak from "@/components/LanguageSpeak/LanguageSpeak";
-
 export default function Home() {
-  return (
-    <div className="h-[5000px]">
-      <LanguageSpeak />
-    </div>
-  );
+  return <div className="">{}</div>;
 }
