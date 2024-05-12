352c0e7bab292b9b8492429e8987fdc3380f6fe5
Tahsin <work.tahsin@gmail.com>
2024-05-12 12:47:24 -0400

    change deprecated and use patter

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com", "picsum.photos", "prod-files-secure.s3.us-west-2.amazonaws.com", "prod-files-secure.s3.us-west-1.amazonaws.com"],
+    remotePatterns: [{ hostname: "source.unsplash.com" }, { hostname: "picsum.photos" }, { hostname: "prod-files-secure.s3.us-west-1.amazonaws.com" }, { hostname: "prod-files-secure.s3.us-west-2.amazonaws.com" }],
   },
 };
 
