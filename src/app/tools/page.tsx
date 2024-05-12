88b12a1f0170022e88f7d751ef10c2007ed615ec
Tahsin <work.tahsin@gmail.com>
2024-05-12 13:42:31 -0400

    add language i speak into tools section

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,12 +1,13 @@
 import Code from "@/app/tools/Code";
 import Tags from "@/components/Blog/Tags";
+import LanguageSpeak from "@/components/LanguageSpeak/LanguageSpeak";
 import services from "@/services";
 import { Tool } from "@/types";
 
 const CategoryBlock = ({ tool }: { tool: Tool }) => {
   return (
     <div className="rounded-sm overflow-hidden border">
-      <div className="bg-slate-900 px-2 py-3">
+      <div className="bg-[rgba(0,0,0,0.1)] px-2 py-3">
         <p className="text-sm">{tool.categoryName}</p>
       </div>
 
@@ -33,6 +34,16 @@ const Page = async () => {
         <div className="mt-5">
           <p className="text-sm text-center">All the technologies, frameworks and tools I use mostly are based on these languages.</p>
         </div>
+      </div>
+
+      <div className="mt-20">
+        <div className="mb-10">
+          <div className="divider">
+            <h2 className="text-lg">Langaugee I speak</h2>
+          </div>
+        </div>
+
+        <LanguageSpeak />
       </div>
 
       <div className="mt-20">
