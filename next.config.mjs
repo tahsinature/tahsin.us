efb9dc799c127510d539b404276d2c8c663609c9
Tahsin <work.tahsin@gmail.com>
2024-05-20 10:42:20 -0400

    allow all image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "source.unsplash.com" }, { hostname: "picsum.photos" }, { hostname: "prod-files-secure.s3.us-west-1.amazonaws.com" }, { hostname: "prod-files-secure.s3.us-west-2.amazonaws.com" }],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
 };
 
