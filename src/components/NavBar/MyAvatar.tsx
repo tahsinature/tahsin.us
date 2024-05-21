5b7f95f8513502a97672f5a2c100d12507fa612d
Tahsin <work.tahsin@gmail.com>
2024-05-21 08:22:29 -0400

    avatar click handle

--- src/components/NavBar/MyAvatar.tsx
+++ src/components/NavBar/MyAvatar.tsx
@@ -2,7 +2,12 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const MyAvatar = () => {
   return (
-    <Avatar className="border-solid border-2  duration-1000 hover:border-gray-400 border-transparent">
+    <Avatar
+      className="border-solid border-2  duration-1000 hover:border-gray-400 border-transparent"
+      onClick={() => {
+        console.log("v1");
+      }}
+    >
       <AvatarImage src="https://github.com/tahsinature.png" />
       <AvatarFallback>MT</AvatarFallback>
     </Avatar>
