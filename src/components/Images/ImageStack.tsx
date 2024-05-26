4998db551b8551d5e54dfbbe2c6e5be5665eee0a
Tahsin <work.tahsin@gmail.com>
2024-05-25 21:45:19 -0400

    fix spacing

--- src/components/Images/ImageStack.tsx
+++ src/components/Images/ImageStack.tsx
@@ -2,9 +2,9 @@ import { Image } from "@nextui-org/react";
 import classes from "./ImageStack.module.scss";
 import clsx from "clsx";
 
-const ImageStack = (props: { images: [string, string, string] }) => {
+const ImageStack = (props: { images: [string, string, string]; className?: string }) => {
   return (
-    <div className={clsx(classes.Holder)}>
+    <div className={clsx(classes.Holder, props.className)}>
       <div className={clsx(classes.StackImage)}>
         <Image radius="none" src={props.images[0]} height={200} width={200} alt="stack of image" />
       </div>
