5d00a800227f2e0e520cea3e6c4443eee423487d
Tahsin <work.tahsin@gmail.com>
2024-06-01 10:50:55 -0400

    add command menu

--- src/components/CommandMenu/CommandMenu.tsx
+++ src/components/CommandMenu/CommandMenu.tsx
@@ -4,6 +4,8 @@ import * as React from "react";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import fonts from "@/lib/fonts";
+import clsx from "clsx";
 
 export default function CommandMenu() {
   const [open, setOpen] = React.useState(false);
@@ -37,10 +39,10 @@ export default function CommandMenu() {
   return (
     <>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput placeholder="Type a command or search..." />
+        <CommandInput className={fonts.calculatorFont.className} placeholder="Type a command or search..." />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup>
+          <CommandGroup className={fonts.calculatorFont.className}>
             <CommandItem onSelect={handleSelect(dict.GOTO_HOME)}>Home</CommandItem>
             <CommandItem onSelect={handleSelect(dict.GOTO_WORKS)}>Works</CommandItem>
             <CommandItem onSelect={handleSelect(dict.GOTO_BLOGS)}>Blogs</CommandItem>
@@ -53,7 +55,9 @@ export default function CommandMenu() {
 
       <Button
         onClick={() => setOpen((open) => !open)}
-        className="inline-flex items-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-8 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
+        className={clsx(
+          "inline-flex items-center whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input hover:bg-accent hover:text-accent-foreground px-4 py-2 relative h-8 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
+        )}
       >
         <span className="hidden lg:inline-flex">Search...</span>
         <span className="inline-flex lg:hidden">Search...</span>
