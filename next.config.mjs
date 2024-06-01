2f62bd3fd6094dbfb8cbb0c4453891c5a9f0191c
Tahsin <work.tahsin@gmail.com>
2024-06-01 10:51:19 -0400

    fix build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+import path from "path";
+
+const __dirname = path.resolve();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+
+    return config;
   },
 };
 
